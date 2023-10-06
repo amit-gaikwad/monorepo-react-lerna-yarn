e943a80c7aed762ac124805cc0b4ddfc71a93374
amit-gaikwad <amitgaikwad795@gmail.com>
2023-10-06 17:26:14 +0530

    add monorepo code for react with lerna craco

---
+++ packages/web/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'; 
+import {Button} from "@amit/common";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <Button textColor={"blue"} background="red">Check this is </Button>
+    </div>
+  );
+}
+
+export default App;
